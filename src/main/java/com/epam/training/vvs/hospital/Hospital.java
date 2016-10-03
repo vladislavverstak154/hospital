ec6591438f37f4c5c5954d8bc554f04b32ae6fa9
hvm-ПК\hvm <vladislavverstak@gmail.com>
2016-10-03 23:13:18 +0300

    Creating a "Hospital" project

---
+++ src/main/java/com/epam/training/vvs/hospital/Hospital.java
@@ -0,0 +1,11 @@
+package com.epam.training.vvs.hospital;
+
+public class Hospital {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to our hospital! If you want to stay alive,");
+		System.out.println("You have to chose another place!:))");
+
+	}
+
+}
