d49dae62b4cfb2bfd70454318c99c6d91eeb799c
hvm-ПК\hvm <vladislavverstak@gmail.com>
2016-10-14 07:14:15 +0300

    Не получается создать классы в импортированном проекте

---
+++ datamodel/src/main/java/com/vvs/training/hospital/datamodel/AbstractModel.java
@@ -0,0 +1,15 @@
+package com.vvs.training.hospital.datamodel;
+
+public class AbstractModel {
+
+    private Long id;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
