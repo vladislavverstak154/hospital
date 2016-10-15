4ef72c1227a24b38805e26b9c8248ff80d9b3366
hvm-ПК\hvm <vladislavverstak@gmail.com>
2016-10-15 13:56:27 +0300

    no message

--- datamodel/src/main/java/com/vvs/training/hospital/datamodel/Drug.java
+++ datamodel/src/main/java/com/vvs/training/hospital/datamodel/Drug.java
@@ -1,5 +1,22 @@
 package com.vvs.training.hospital.datamodel;
 
-public class Drug extends AbstractModel {
+import java.sql.Date;
 
+public class Drug extends AbstractModel {
+	private String recipe;
+	private Date endDate;
+	public String getRecipe() {
+		return recipe;
+	}
+	public void setRecipe(String recipe) {
+		this.recipe = recipe;
+	}
+	public Date getEndDate() {
+		return endDate;
+	}
+	public void setEndDate(Date endDate) {
+		this.endDate = endDate;
+	}
+	
+	
 }
--- datamodel/src/main/java/com/vvs/training/hospital/datamodel/Operation.java
+++ datamodel/src/main/java/com/vvs/training/hospital/datamodel/Operation.java
@@ -1,5 +1,21 @@
 package com.vvs.training.hospital.datamodel;
 
-public class Operation extends AbstractModel {
+import java.sql.Date;
 
+public class Operation extends AbstractModel {
+	private String title;
+	private Date datePerform;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getDatePerform() {
+		return datePerform;
+	}
+	public void setDatePerform(Date datePerform) {
+		this.datePerform = datePerform;
+	}
+	
 }
