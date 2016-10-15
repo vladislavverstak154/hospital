8242735eb4482c521d0d8d234f8589f57aaecdbd
hvm-ПК\hvm <vladislavverstak@gmail.com>
2016-10-15 13:54:33 +0300

    no message

--- datamodel/src/main/java/com/vvs/training/hospital/datamodel/Procedure.java
+++ datamodel/src/main/java/com/vvs/training/hospital/datamodel/Procedure.java
@@ -1,5 +1,20 @@
 package com.vvs.training.hospital.datamodel;
 
-public class Procedure extends AbstractModel {
+import java.sql.Date;
 
+public class Procedure extends AbstractModel {
+	private String title;
+	private Date dateEnd;
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	public Date getDateEnd() {
+		return dateEnd;
+	}
+	public void setDateEnd(Date dateEnd) {
+		this.dateEnd = dateEnd;
+	}
 }
